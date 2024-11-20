4da6a5c059b98ecec88c29b63192b96fe704bdae
Anmol Gupta <anmol.gupta@rsk-bsl.com>
2024-11-20 17:53:26 +0530

    Newman Script Generator Done

--- newmanScriptGenerator.js
+++ newmanScriptGenerator.js
@@ -1,11 +1,68 @@
-var token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjJEMDQwODBDNUNBQzhCNEZEQjAyRDEwRDBBOTFCOERBREQzQzBFRDUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJMUVFJREZ5c2kwX2JBdEVOQ3BHNDJ0MDhEdFUifQ.eyJuYmYiOjE3MzEwNjEzNzgsImV4cCI6MTczMTA2MjI3OCwiaXNzIjoiaHR0cHM6Ly9pZGVudGl0eS5lbWVhMWIuZmVuZXJnb3guY29tIiwiYXVkIjoiRmVuZXJnby5OZWJ1bGEuQXV0aG9yaXplciIsImNsaWVudF9pZCI6InZpc3RyYS1zZGxjLWRldiIsImNsaWVudF90ZW5hbnQiOiIxY2YxYmY2ZS02Y2EzLTRmODMtOTZmNS0wNmU3Y2U2NDJlYTYiLCJpYXQiOjE3MzEwNjEzNzgsInNjb3BlIjpbImZlbnguYWxsIl19.i33bnsFqFTN91dtLEqkedIAVDpaGsWS8ogmVb2aLmTDclWKSm3dX-imNNcx6xfkw6MYqmHLYeDcrBjcN3dWTZEfl3ezoqOzlgiLavUOSzUj2cIMGkxLlkUKunPHt2OnvxmsnNhAPV6DZBgAcn5Kaflj6kiZHDqFFSLgv0X1P0BTsg-A5wJnstTR6aHYjL0qSBLdzluTRwNaHl57BjOFpQorVq32R4EizfBXHWPqDo-tTSDjlliSRNKQkrxDgw-IQ9S67sfhuc2gFoEOEZhvG8a5ywxUzRBfb8udIAqQRukjmb6Vypi4jtR-ErG29vF_NGYtjVMMhNbZD5S62TtKMkg'
+import { normalize } from 'path';
+import { exec } from "child_process";
+import { error } from 'console';
+import { stdout, stderr } from 'process';
 
-var companyCollectionPath = ''
-var indidualCollectionPath = ''
 
-var envFile = ''
+function genrateCommand(collectionFilePath, envFilePath, envVariables, iterationDataFilePath, reporters){
 
-var iterationDataForIndividual = ''
-var iterationDataForCompany = ''
 
-console.log()
+   const normalizedCollectionFilePath = normalize(collectionFilePath);
+   const normalizedEnvFilePath = normalize(envFilePath);    
+   const normalizedIterationDataFilePath = normalize(iterationDataFilePath);
+
+
+    let command = `newman run "${normalizedCollectionFilePath}" --environment "${normalizedEnvFilePath}"` ;
+
+    for(const[key, value] of Object.entries(envVariables)) {
+        command += ` --env-var ${key}=${value}`
+    }
+
+    command += ` --iteration-data "${normalizedIterationDataFilePath}" --reporters ${reporters} `
+
+
+    return command;
+
+
+}
+
+
+function executeCommand(generatedCommand){
+
+    exec(generatedCommand, (error,stdout,stderr)=>{
+        if(error){
+            console.error(`Error executing the command ${error}`);
+            
+        }
+
+        if(stderr){
+            console.error(`Command Produced error : ${stderr}`);
+            
+        }
+
+        console.log("Newman Command Output: ")
+        console.log(stdout);
+        
+    })
+
+}
+
+const collectionFilePath = "C:/ANMOL/Fenergo Project/Data Migration/Automtion Scripts/Output/Collection/CompanyRecordsLocation/Fenergo Migration - Company Record Location Validation.postman_collection.json";
+const envFilePath = "C:/ANMOL/Fenergo Project/Data Migration/Automtion Scripts/Output/Env Files/Fenergo-DEV.postman_environment.json";
+const envVariables = {
+    "client_id" : "vistra-sdlc-dev",
+    "client_secret" : "31c690fd-4955-ca52-5311-30a08993bde1",
+    "token" : "Enter Token here."
+};
+const iterationDataFilePath = "C:/ANMOL/Fenergo Project/Data Migration/Automtion Scripts/Inputs Files/CompanyRecordLocationTesting2.csv";
+const reporters = "htmlextra";
+
+
+const newmanCommand = genrateCommand(collectionFilePath, envFilePath, envVariables, iterationDataFilePath, reporters)
+
+
+
+console.log("\n" + newmanCommand +"\n");
+
+
+executeCommand(newmanCommand)
